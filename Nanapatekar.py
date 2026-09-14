ecdecaf483e664c88eb34b1c9ea95164c53bb829
Nayan Sarkar <sarnayan22@gmail.com>
2026-09-14 14:13:34 +0530

    My trial commite

---
+++ Nanapatekar.py
@@ -0,0 +1,10 @@
+print("Hii well come to python world")
+print("You Can learn the Best Coding Language and Programing By MR. Suman Mandal Sir")
+print("Example")
+# calculator
+# Find circle
+r=float(input("Enter any radious number : "))
+A=3.14*r*r
+print("The redious of a circle is :",A)
+
+# you can find the result 
